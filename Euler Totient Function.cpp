0f7960597ba0b50eb34f80fbba58ddb381af0b0b
Sami Emad <admin@samiemad.me>
2017-04-17 13:02:36 +0300

    Add factor implementation

--- Euler Totient Function.cpp
+++ Euler Totient Function.cpp
@@ -1,3 +1,18 @@
+vector<int> factor(int n) {
+	vector<int> f;
+	for(int i=2; i*i<=n; ++i) {
+		if(n%i==0) {
+			f.push_back(i);
+			while(n%i==0) {
+				n/=i;
+			}
+		}
+	}
+	if(n!=1)
+		f.push_back(n);
+	return f;
+}
+
 int phi(int n) {
 	vector<int> p = factor(n);
 	for (int i = 0; i < (int) p.size(); i++) {
@@ -8,4 +23,3 @@ int phi(int n) {
 	}
 	return n;
 }
-// TODO: add factor() implementation
